ff29ab51c7d738bbe5f6b3793cabc74ea957a57b
detmarson <124086393+detmarson@users.noreply.github.com>
2023-09-27 16:13:46 +0000

    added some media queries

--- src/components/BlogItem/BlogItem.jsx
+++ src/components/BlogItem/BlogItem.jsx
@@ -4,15 +4,18 @@ import '@coreui/coreui/dist/css/coreui.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { createDescription, stripHTML } from '/src/util/StringUtils.js'
 import './BlogItem.css'
+import { useMediaQuery } from 'react-responsive'
+
 
 export default function BlogItem({ title, summary, thumbnail, index, id }) {
+	const isSmallScreen = useMediaQuery({ query: '(max-width: 991.98px)' })
 	return (
 		<>
 			<CCol xs>
 				<CCard
 					style={{
 						borderRadius: '2rem',
-						left: index % 2 === 0 ? '20%' : '-20%',
+						left: isSmallScreen ? '0%' : index % 2 === 0 ? '20%' : '-20%',
 					}}>
 					<Link to={`/posts/${id}`}>
 						<CCardImage orientation="top" src={thumbnail} />
